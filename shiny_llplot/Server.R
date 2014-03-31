ddc866b9f7e6c5d779a773ded07c1298d7897723
Crt Ahlin <crt.ahlin@gmail.com>
2014-03-31 15:08:36 +0200

    Date picekr for filtering data now works.

--- shiny_llplot/Server.R
+++ shiny_llplot/Server.R
@@ -15,13 +15,16 @@ shinyServer( function(input, output, session) {
   dataHP <- reactive(function(){
     # print(input$dataFileAIRS)
     data <- AIRSImportMultiple(dataFilesAIRS=input$dataFileAIRS, sensor="HP")  
+
     return(data)
-    browser()
-    print(data)
     })
   
   dataFitbit <- reactive(function(){
     data <- read.csv(file=input$dataFileFitbit$datapath)  
+    data <- data[,c("date", "steps", "minutesAsleep", "awakeningsCount")]
+    # browser()
+    data$date <- format(data$date, format="%d.%m.%Y")
+    #data[,"date"] <- as.Date(data[,"date"], format="%d.%m.%Y")
     return(data)
   })
   
@@ -31,11 +34,20 @@ shinyServer( function(input, output, session) {
   output$plot <- renderPlot(expr={hist(dataHP()$Value, breaks=50)})
   output$plotHP <- renderPlot(
     expr={
-      p <- ggplot(data=dataHP()) + 
+      dataFiltered <- subset(dataHP(), subset={(as.Date(dataHP()$Day, format="%d.%m.%Y") >= input$dateRange[1]) &
+                                                 (as.Date(dataHP()$Day, format="%d.%m.%Y") <= input$dateRange[2]) })
+      p <- ggplot(data=dataFiltered) + 
         theme_bw() + 
         geom_point(aes(x=CyclicTime, y=Value)) + 
         scale_x_continuous(limits=c(0,1)) +
         geom_smooth(aes(x=CyclicTime, y=Value))
+      # faceting by weekday
+      if (input$selectedFacet == "weekdayFacet") { 
+        p <- p + facet_grid(Weekday ~ .)}
+      # faceting by day
+      if (input$selectedFacet == "dayFacet") { 
+        p <- p + facet_grid(Day ~ .)}
+      
       print(p)
     }
     )
--- shiny_llplot/Ui.R
+++ shiny_llplot/Ui.R
@@ -15,28 +15,27 @@ shinyUI(
       selectInput(inputId="selectedFormat",
                   label="Select data format",
                   selected="AIRS", 
-                  choices=c("AIRS CSV"="AIRS",
-                            "CSV (not supported)"="CSV")),
+                  choices=c("AIRS CSV"="AIRS")),
       
       fileInput(inputId="dataFileAIRS", label="HR data file", multiple=TRUE),
       
       dateRangeInput(inputId="dateRange", 
                      label="Date range",
-                     start=2013-01-01,
+                     start="2013-01-01",
                      format="d.m.yyyy" ),
       
       selectInput(inputId="selectedFacet",
                   label="Faceting by", choices=c(
                     "No faceting"="noFacet",
-                    "Day of the week"="weekdayWeekFacet",
+                    "Day of the week"="weekdayFacet",
                     "Day"="dayFacet" ),
                   selected="noFacet",
                   multiple=FALSE),
       
       selectInput(inputId="selectedPlotMethod",
                   label="Select plotting method",
-                  choices=c("Cyclic cubic polynomial"="cycCubPoly",
-                            "Cyclic fractional polynomial"="cycFracPoly",
+                  choices=c("Cyclic cubic polynomial (not implemented)"="cycCubPoly",
+                            "Cyclic fractional polynomial (not implemented)"="cycFracPoly",
                             "Cyclic GAM smoother"="cycGAMSmooth"),
                   selected="cycGAMSmooth", multiple=TRUE),
       
@@ -73,7 +72,7 @@ shinyUI(
                tableOutput("dataHP")),
         
         tabPanel(title="Fitbit data",
-               p("Some data points."),
+               p("First 100 data points."),
                tableOutput("dataFitbit")
                )
         )
