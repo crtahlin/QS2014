416366c69355fc1bc8a9c71659a1f5e50b2fa8c5
Crt Ahlin <crt.ahlin@gmail.com>
2014-03-31 16:46:57 +0200

    Faceting now works with Fitbit data displayed (when faceting by day).

--- shiny_llplot/Server.R
+++ shiny_llplot/Server.R
@@ -15,28 +15,55 @@ shinyServer( function(input, output, session) {
   dataHP <- reactive(function(){
     # print(input$dataFileAIRS)
     data <- AIRSImportMultiple(dataFilesAIRS=input$dataFileAIRS, sensor="HP")  
-
+data$Day <- as.Date(data$Day, format="%d.%m.%Y")
     return(data)
     })
+  
+  
+  dataHPFiltered <- reactive({
+    data <- subset(dataHP(),
+           subset={(as.Date(dataHP()$Day, format="%d.%m.%Y") >= input$dateRange[1]) &
+                     (as.Date(dataHP()$Day, format="%d.%m.%Y") <= input$dateRange[2]) })
+    print(str(data))
+    return(data)
+    })
+  
   
   dataFitbit <- reactive(function(){
     data <- read.csv(file=input$dataFileFitbit$datapath)  
     data <- data[,c("date", "steps", "minutesAsleep", "awakeningsCount")]
     # browser()
-    data$date <- format(data$date, format="%d.%m.%Y")
+    data$date <- as.Date(data$date, format="%d.%m.%Y")
     #data[,"date"] <- as.Date(data[,"date"], format="%d.%m.%Y")
+    colnames(data)[which(colnames(data)=="date")] <- "Day"
     return(data)
   })
   
-  output$dataHP <- renderTable( dataHP()[sample(seq(dim(dataHP())[1]), size=100),] )
-  output$dataFitbit <- renderTable(head(dataFitbit(), n=100))
+  dataFitbitFiltered <- reactive ({
+    # only take those days for which HR data exists?
+    # TODO
+    dayswithHR <- unique(dataHPFiltered()$Day)
+    #data <- subset(dataFitbit(),
+    #       subset={(as.Date(dataFitbit()$Day, format="%d.%m.%Y") >= input$dateRange[1]) &
+    #                 (as.Date(dataFitbit()$Day, format="%d.%m.%Y") <= input$dateRange[2]) })
+    
+    #TODO - do proper filtering!
+    data <- subset(dataFitbit(), subset={dataFitbit()$Day==dayswithHR})
+    # data <- dataFitbit()["Day"==dayswithHR,]
+    print(str(data))
+    return(data)
+    })
+  
+  output$dataHP <- renderTable( dataHP()[sample(seq(dim(dataHP())[1]), size=100),
+                                         c("Value", "CyclicTime", "Weekday", "Day")] )
+  
+  output$dataFitbit <- renderTable(head(dataFitbitFiltered(), n=100))
   
   output$plot <- renderPlot(expr={hist(dataHP()$Value, breaks=50)})
   output$plotHP <- renderPlot(
     expr={
-      dataFiltered <- subset(dataHP(), subset={(as.Date(dataHP()$Day, format="%d.%m.%Y") >= input$dateRange[1]) &
-                                                 (as.Date(dataHP()$Day, format="%d.%m.%Y") <= input$dateRange[2]) })
-      p <- ggplot(data=dataFiltered) + 
+      
+      p <- ggplot(data=dataHPFiltered()) + 
         theme_bw() + 
         geom_point(aes(x=CyclicTime, y=Value)) + 
         scale_x_continuous(limits=c(0,1)) +
@@ -47,6 +74,13 @@ shinyServer( function(input, output, session) {
       # faceting by day
       if (input$selectedFacet == "dayFacet") { 
         p <- p + facet_grid(Day ~ .)}
+      
+      # testing adding fitbit data
+      if (input$selectedFacet == "dayFacet") { 
+      p <- p + geom_text(data=dataFitbitFiltered(), aes(x=0.15, y=40, label=paste("Steps count:", steps)))
+      # p <- p + annotate("text", label=dataFitbitFiltered()$steps, x=0, y=0)
+      
+      }
       
       print(p)
     }
